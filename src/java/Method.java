f6036a0e66d95e13b76da98ce5495e44280d9d56
kaushy <kaushikkumar.k27@gmail.com>
2024-06-11 17:36:43 +0530

    smallest/average

--- src/java/Method.java
+++ src/java/Method.java
@@ -1,4 +1,4 @@
-class Method{
+/*class Method{
     //finding the smmalest value
     static int smallest(int x,int y,int z){
         return Math.min(Math.min(x,y),z);
@@ -10,5 +10,16 @@ class Method{
         int z=40;
         System.out.println(smallest(x,y,z));
         
+    }
+} */
+class Method{
+    static int avrage(int x, int y, int z){
+        return(x+y+z)/3;
+    }
+    public static void main(String[]args){
+        int x=10;
+        int y=20;
+        int z=30;
+        System.out.println(avrage(x, y, z));
     }
 }
