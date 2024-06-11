1024bd9cdc429277052795eb087efe969cb56da6
kaushy <kaushikkumar.k27@gmail.com>
2024-06-11 18:26:58 +0530

    smallest/avrage/leapyear

--- src/java/Method.java
+++ src/java/Method.java
@@ -12,7 +12,7 @@
         
     }
 } */
-class Method{
+/*class Method{
     static int avrage(int x, int y, int z){
         return(x+y+z)/3;
     }
@@ -22,4 +22,32 @@ class Method{
         int z=30;
         System.out.println(avrage(x, y, z));
     }
+}*/
+import java.util.Scanner;
+
+class Method {
+ 
+ public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the year");
+        int year = sc.nextInt();
+
+        System.out.println(is_LeapYear(year));
+    }
+ 
+ public static boolean is_LeapYear(int y)
+    {
+        if ((y % 4) == 0) {
+            if ((y % 100) != 0 || ((y % 100 == 0) && (y % 400 == 0))) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        
+    }
 }
+
