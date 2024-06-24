32784dc08c6e29f2c82f5a23ed5fbd42d1898eb8
Kaushy <98571746+Kaushy@users.noreply.github.com>
2024-06-24 14:26:40 +0530

    Creating a simple banking system where user can deposit and withdraw money from a shared bank account.

---
+++ src/java/bank/Final.java
@@ -0,0 +1,14 @@
+package bank;
+public class Final extends BankAccount {
+    public static void main(String[] args) {
+        Process();
+
+    }
+}
+class InsufficientFundsException extends Exception {
+//    double amount;
+    InsufficientFundsException(double bal) {
+       // amount=amt;
+
+    }
+}
