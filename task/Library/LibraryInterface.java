5ebccae635a64a3f3a258a00ad4e36a27506b893
Kaushy <98571746+Kaushy@users.noreply.github.com>
2024-06-25 17:56:19 +0530

    Delete task/Library directory

--- task/Library/LibraryInterface.java
+++
@@ -1,9 +0,0 @@
-package Library;
-
-interface LibraryInterface {
-        public void addBook(Book book); // interface method (does not have a body)
-        public void removeBook(String isbn);
-        public Book searchBook(String Title);
-        public void getAllDettailsofBook();
-
-}
