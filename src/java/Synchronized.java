76e2028b57bf7c95b20368300e3dd6ebb750bb8f
Kaushy <98571746+Kaushy@users.noreply.github.com>
2024-06-26 16:09:41 +0530

    Synchronized Thread

---
+++ src/java/Synchronized.java
@@ -0,0 +1,36 @@
+class CakeCounter{
+    int counter=0;
+    public synchronized void increment(){
+        counter++;
+    }
+}
+class Team implements Runnable{
+    CakeCounter counter;
+    public Team(CakeCounter counter){
+        this.counter=counter;
+    }
+    public void run(){
+        for(int i=0;i<10;i++){
+            counter.increment();
+        }
+    }
+}
+
+class Synchronized{
+    public static void main(String[]args){
+        CakeCounter counter=new CakeCounter();
+        Thread team1=new Thread(new Team(counter));
+        Thread team2=new Thread(new Team(counter));
+
+        team1.start();
+        team2.start();
+        try{
+            team1.join();
+            team2.join();
+        }
+        catch (Exception e){
+
+        }
+        System.out.println(counter.counter);
+    }
+}
